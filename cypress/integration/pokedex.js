631720360c79e483c7d6d57da0858335a312474c
Diego Romero <diegoromeropersonal@gmail.com>
2021-11-23 22:27:53 -0300

    cypress test with ci

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,8 +1,16 @@
 /* eslint-disable no-undef */
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(() => {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can navigate to pokemons', () => {
+    cy.contains('pidgey').click()
+    cy.contains('keen eye')
   })
 })
